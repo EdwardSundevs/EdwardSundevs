17b01455aa217d080eb0652cf0eca8cef6f590b4
edward abreu <eabreu@xirectds.com>
2022-02-24 19:44:44 -0400

    Test cafe rio 1

--- Libs/CustomKeywords.groovy
+++ Libs/CustomKeywords.groovy
@@ -3,3 +3,92 @@
  * This class is generated automatically by Katalon Studio and should not be modified or deleted.
  */
 
+import com.kms.katalon.core.testobject.TestObject
+
+import java.lang.String
+
+import com.applitools.eyes.selenium.Eyes
+
+import org.openqa.selenium.WebElement
+
+import com.applitools.eyes.RectangleSize
+
+
+ /**
+	 * Check if element present in timeout
+	 * @param to Katalon test object
+	 * @param timeout time to wait for element to show up
+	 * @return true if element present, otherwise false
+	 */ 
+def static "steps_SignUp.TEst.isElementPresent_Mobile"(
+    	TestObject to	
+     , 	int timeout	) {
+    (new steps_SignUp.TEst()).isElementPresent_Mobile(
+        	to
+         , 	timeout)
+}
+
+ /**
+	 * Get mobile driver for current session
+	 * @return mobile driver for current session
+	 */ 
+def static "steps_SignUp.TEst.getCurrentSessionMobileDriver"() {
+    (new steps_SignUp.TEst()).getCurrentSessionMobileDriver()
+}
+
+
+def static "com.kms.katalon.keyword.applitools.BasicKeywords.checkTestObject"(
+    	TestObject testObject	
+     , 	String testName	) {
+    (new com.kms.katalon.keyword.applitools.BasicKeywords()).checkTestObject(
+        	testObject
+         , 	testName)
+}
+
+
+def static "com.kms.katalon.keyword.applitools.BasicKeywords.checkWindow"(
+    	String testName	) {
+    (new com.kms.katalon.keyword.applitools.BasicKeywords()).checkWindow(
+        	testName)
+}
+
+
+def static "com.kms.katalon.keyword.applitools.BasicKeywords.checkElement"(
+    	Eyes eyes	
+     , 	WebElement element	) {
+    (new com.kms.katalon.keyword.applitools.BasicKeywords()).checkElement(
+        	eyes
+         , 	element)
+}
+
+
+def static "com.kms.katalon.keyword.applitools.EyesKeywords.eyesOpen"(
+    	String testName	
+     , 	RectangleSize viewportSize	) {
+    (new com.kms.katalon.keyword.applitools.EyesKeywords()).eyesOpen(
+        	testName
+         , 	viewportSize)
+}
+
+
+def static "com.kms.katalon.keyword.applitools.EyesKeywords.eyesClose"(
+    	Eyes eyes	) {
+    (new com.kms.katalon.keyword.applitools.EyesKeywords()).eyesClose(
+        	eyes)
+}
+
+
+def static "com.kms.katalon.keyword.applitools.EyesKeywords.eyesInit"() {
+    (new com.kms.katalon.keyword.applitools.EyesKeywords()).eyesInit()
+}
+
+
+def static "com.kms.katalon.keyword.applitools.EyesKeywords.eyesOpenWithBaseline"(
+    	String baselineName	
+     , 	String testName	
+     , 	RectangleSize viewportSize	) {
+    (new com.kms.katalon.keyword.applitools.EyesKeywords()).eyesOpenWithBaseline(
+        	baselineName
+         , 	testName
+         , 	viewportSize)
+}
--- Libs/internal/GlobalVariable.groovy
+++ Libs/internal/GlobalVariable.groovy
@@ -1,17 +1,43 @@
 package internal
 
 import com.kms.katalon.core.configuration.RunConfiguration
-import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
-import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
-import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import com.kms.katalon.core.main.TestCaseMain
+
 
 /**
  * This class is generated automatically by Katalon Studio and should not be modified or deleted.
  */
 public class GlobalVariable {
+     
+    /**
+     * <p>Profile CafeRio_Dev : com.punchh.caferio</p>
+     */
+    public static Object cafeRio_dev
+     
+    /**
+     * <p></p>
+     */
+    public static Object User_Email_Dev
+     
+    /**
+     * <p></p>
+     */
+    public static Object Password_Dev
+     
+    /**
+     * <p>Profile CafeRio_Prod : com.punchh.caferio</p>
+     */
+    public static Object cafeRio_Prod
+     
+    /**
+     * <p></p>
+     */
+    public static Object UserEmail_Prod
+     
+    /**
+     * <p></p>
+     */
+    public static Object Password_Prod
      
     /**
      * <p></p>
@@ -33,18 +59,32 @@ public class GlobalVariable {
      */
     public static Object G_ShortTimeOut
      
+    /**
+     * <p></p>
+     */
+    public static Object caferio_dev
+     
 
     static {
-        def allVariables = [:]        
-        allVariables.put('default', ['G_Timeout' : 10, 'G_NotificationMessage' : 'Your message has been sent. View message', 'G_AndroidApp' : 'androidapp/APIDemos.apk', 'G_ShortTimeOut' : 5])
-        
-        String profileName = RunConfiguration.getExecutionProfile()
-        
-        def selectedVariables = allVariables[profileName]
-        G_Timeout = selectedVariables['G_Timeout']
-        G_NotificationMessage = selectedVariables['G_NotificationMessage']
-        G_AndroidApp = selectedVariables['G_AndroidApp']
-        G_ShortTimeOut = selectedVariables['G_ShortTimeOut']
-        
+        try {
+            def selectedVariables = TestCaseMain.getGlobalVariables("default")
+			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+            selectedVariables += TestCaseMain.getParsedValues(RunConfiguration.getOverridingParameters())
+    
+            cafeRio_dev = selectedVariables['cafeRio_dev']
+            User_Email_Dev = selectedVariables['User_Email_Dev']
+            Password_Dev = selectedVariables['Password_Dev']
+            cafeRio_Prod = selectedVariables['cafeRio_Prod']
+            UserEmail_Prod = selectedVariables['UserEmail_Prod']
+            Password_Prod = selectedVariables['Password_Prod']
+            G_Timeout = selectedVariables['G_Timeout']
+            G_NotificationMessage = selectedVariables['G_NotificationMessage']
+            G_AndroidApp = selectedVariables['G_AndroidApp']
+            G_ShortTimeOut = selectedVariables['G_ShortTimeOut']
+            caferio_dev = selectedVariables['caferio_dev']
+            
+        } catch (Exception e) {
+            TestCaseMain.logGlobalVariableError(e)
+        }
     }
 }
